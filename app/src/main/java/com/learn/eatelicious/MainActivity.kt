001129a8b2676e973ad5087677a0adc799b5d38d
DeeKay02 <48171972+deekay02@users.noreply.github.com>
2024-09-12 11:09:44 +0530

    Add restaurant list

--- app/src/main/java/com/learn/eatelicious/MainActivity.kt
+++ app/src/main/java/com/learn/eatelicious/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learn.eatelicious.ui.theme.EateliciousTheme
+
+data class Restaurant(val name: String, val tagLine: String)
+
+val restaurants = listOf(
+    Restaurant("Belgian Waffles", "The best in town!"),
+    Restaurant("Stomachful", "Never leave hungry"),
+    Restaurant("Big Belly Noodles", "Delicious noodles"),
+    Restaurant("Cakery", "Cakes for every occasion"),
+    Restaurant("Pan Asia", "The best Asian food"),
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +33,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EateliciousTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RestaurantList(restaurants = restaurants)
                 }
             }
         }
@@ -31,17 +45,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RestaurantCard(restaurant: Restaurant) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        shadowElevation = 3.dp,
+        color = Color.LightGray
+    ) {
+        Column {
+            Text(
+                text = restaurant.name,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 32.dp, bottom = 2.dp)
+            )
+            Text(
+                text = restaurant.tagLine,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 2.dp, bottom = 32.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RestaurantList(restaurants: List<Restaurant>) {
+    Column {
+        for (restaurant in restaurants) {
+            RestaurantCard(restaurant = restaurant)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewRestaurantCard() {
     EateliciousTheme {
-        Greeting("Android")
+        RestaurantCard(
+            Restaurant(
+                "Belgian Waffles",
+                "For the sweet-tooth's!"
+            )
+        )
     }
 }
